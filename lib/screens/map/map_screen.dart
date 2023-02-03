c1a97b480422284230bc2f1e3a2bb09020ef6b10
Anton Tursenev <rikishi@rikishi-pro.local>
2023-02-03 22:52:08 +0700

    Update map layers

--- lib/screens/map/map_screen.dart
+++ lib/screens/map/map_screen.dart
@@ -97,30 +97,100 @@ class MapScreen extends StatelessWidget {
   }
 
   Widget _body(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(51.5, -0.09),
-        zoom: 5,
-        maxZoom: 17,
-        minZoom: 5,
-        interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
-      ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'guide.traveltime.app',
-          retinaMode: false, // MediaQuery.of(context).devicePixelRatio > 1.0,
+    return Stack(alignment: Alignment.topCenter, children: [
+      FlutterMap(
+        options: MapOptions(
+          center: LatLng(51.5, -0.09),
+          zoom: 3,
+          maxZoom: 17,
+          minZoom: 3,
+          interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+          // onTap: (LatLng location) {},
+          // onPositionChanged: (MapPosition position, bool hasGesture) {},
         ),
-      ],
-    );
+        children: [
+          TileLayer(
+            // urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: const ['a', 'b', 'c'],
+            userAgentPackageName: 'guide.traveltime.app',
+            retinaMode: false, // MediaQuery.of(context).devicePixelRatio > 1.0,
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(51.5, -0.09),
+                width: 40,
+                height: 40,
+                builder: (_) => const Icon(Icons.location_on, size: 40),
+                anchorPos: AnchorPos.align(AnchorAlign.top),
+              ),
+            ],
+          ),
+          PolygonLayer(
+            polygonCulling: false,
+            polygons: [
+              Polygon(
+                points: [
+                  LatLng(30, 40),
+                  LatLng(20, 50),
+                  LatLng(25, 45),
+                ],
+                color: Colors.blue,
+              ),
+            ],
+          ),
+          PolylineLayer(
+            polylineCulling: false,
+            polylines: [
+              Polyline(
+                points: [
+                  LatLng(30, 40),
+                  LatLng(20, 50),
+                  LatLng(25, 45),
+                ],
+                color: Colors.blue,
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).canvasColor.withOpacity(0.75),
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      topRight: Radius.circular(10.0))),
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 102, top: 1),
+                child: Row(
+                  // mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'flutter_map | © ',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    Text(
+                      'OpenStreetMap contributors',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      // const Text('flutter_map | © OpenStreetMap contributors'),
+    ]);
   }
 
   Widget _panel(BuildContext context, ScrollController sc) {
     return MediaQuery.removePadding(
         context: context,
         removeTop: true,
-        child: ListView(controller: sc, children: <Widget>[
-          // Text("This is the sliding Widget"),
-        ]));
+        child:
+            ListView(controller: sc, children: const <Widget>[Text('asdasd')]));
   }
 }
