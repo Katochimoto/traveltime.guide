df52f4bd5199e87fecf359f9435a1a8af0b9f408
Anton Tursenev <rikishi@rikishi-pro.local>
2023-03-13 21:37:07 +0700

    Display list of events per day

---
+++ lib/screens/events/events_list.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:traveltime/constants/routes.dart';
+import 'package:traveltime/widgets/card/card_horizontal.dart';
+import 'package:traveltime/widgets/not_found.dart';
+import 'package:traveltime/store/models/event.dart';
+
+class EventsList extends StatelessWidget {
+  final List<Event> events;
+
+  const EventsList({super.key, required this.events});
+
+  @override
+  Widget build(BuildContext context) {
+    if (events.isEmpty) {
+      return const NotFound();
+    }
+
+    return ListView.builder(
+      itemCount: events.length,
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (_, idx) {
+        final event = events[idx];
+        return CardHorizontal(
+            title: event.title,
+            details: event.intro,
+            img: event.logoImg,
+            tap: () {
+              context.pushNamed(Routes.article,
+                  params: {'id': event.isarId.toString()});
+            });
+      },
+    );
+  }
+}
