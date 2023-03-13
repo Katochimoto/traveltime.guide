a803b8ccb216f2a7347332b5791e7098006c5db1
Anton Tursenev <rikishi@rikishi-pro.local>
2023-03-13 21:37:01 +0700

    Display list of events per day

--- lib/screens/events/events_controller.dart
+++ lib/screens/events/events_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:traveltime/screens/events/events_calendar.dart';
+import 'package:traveltime/screens/events/events_list.dart';
 import 'package:traveltime/store/db.dart';
 import 'package:traveltime/widgets/not_found.dart';
 
@@ -12,7 +13,15 @@ class EventsController extends ConsumerWidget {
     final events = ref.watch(eventsProvider);
     return events.when(
       data: (data) {
-        return data.isEmpty ? const NotFound() : EventsCalendar(events: data);
+        return data.isEmpty
+            ? const NotFound()
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  EventsCalendar(events: data),
+                  Expanded(child: EventsList(events: data)),
+                ],
+              );
       },
       error: (error, stackTrace) {
         return const NotFound();
