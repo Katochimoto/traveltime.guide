82d3e0b89f17de2336761adeec6a467d004ad657
Anton Tursenev <anton.tursenev@ruptela.com>
2023-04-25 11:02:23 +0700

    Displayy polyline

--- lib/screens/map/map_screen_body.dart
+++ lib/screens/map/map_screen_body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
+import 'package:flutter_map_tappable_polyline/flutter_map_tappable_polyline.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:traveltime/providers/map_follow_location.dart';
 import 'package:traveltime/providers/map_tap_position.dart';
 import 'package:traveltime/screens/map/map_current_location.dart';
@@ -106,6 +108,53 @@ class MapScreenBody extends ConsumerWidget {
             // ),
             const Attribution(),
             const Popover(),
+            TappablePolylineLayer(
+              polylineCulling: true,
+              pointerDistanceTolerance: 20,
+              polylines: [
+                TaggedPolyline(
+                  tag: 'My Polyline',
+                  points: [
+                    LatLng(5.58213, 45.13065),
+                    LatLng(5.58209, 45.13078),
+                    LatLng(5.58186, 45.13091),
+                    LatLng(5.58184, 45.13099),
+                  ],
+                  color: Colors.red,
+                  strokeWidth: 9.0,
+                ),
+                TaggedPolyline(
+                  tag: 'My 2nd Polyline',
+                  points: [
+                    LatLng(5.58178, 45.13105),
+                    LatLng(5.5817, 45.13109),
+                    LatLng(5.58163, 45.1311),
+                    LatLng(5.58149, 45.13108),
+                  ],
+                  color: Colors.black,
+                  strokeWidth: 3.0,
+                ),
+                TaggedPolyline(
+                  tag: 'My 3rd Polyline',
+                  points: [
+                    LatLng(5.58141, 45.13103),
+                    LatLng(5.58138, 45.13099),
+                    LatLng(5.58136, 45.13093),
+                    LatLng(5.58125, 45.13084),
+                  ],
+                  color: Colors.blue,
+                  strokeWidth: 3.0,
+                ),
+              ],
+              onTap: (polylines, tapPosition) => print('Tapped: ' +
+                  polylines.map((polyline) => polyline.tag).join(',') +
+                  ' at ' +
+                  tapPosition.globalPosition.toString()),
+              onMiss: (tapPosition) {
+                print('No polyline was tapped at position ' +
+                    tapPosition.globalPosition.toString());
+              },
+            ),
           ],
         ),
         const MapTilesSelect(),
